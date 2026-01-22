bf7a482a6c21320f89d997cb82ca54626cc7d8a7
TimofeiChoi <chie.tyu@edu.spbstu.ru>
2026-01-22 23:08:51 +0300

    лабораторная работа 4

---
+++ лабораторная работа 4/работа 2/main.py
@@ -0,0 +1,21 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+def task() -> None:
+    with open(INPUT_FILENAME, 'r', encoding='utf-8') as f:
+        a = csv.DictReader(f)
+        data = list(a)
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    task()
+    with open(OUTPUT_FILENAME, 'r', encoding='utf-8') as output_f:
+        for line in output_f:
+            print(line, end="")
+
+
